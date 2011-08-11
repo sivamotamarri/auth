be58ae7b6a79ca16237b182d70a50cf233b2f55d
mskrishna <krishna@highonrails.com>
2011-08-11 12:30:21 +0530

    basic auth app in rails 3'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = 'b4da7f15cc3b395f0d695a8bdea128b6796e820ae5d49e7e0e4c0f61fe3b8dcc00c7f79144c64b5bc3bf16c0a8ce9f15c4da41d9160da30fd3ee6e71021b1e25'
